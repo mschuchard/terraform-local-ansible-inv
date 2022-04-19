6fb01fb0277b3b4c2fe3ef29920a992db7989534
Matt Schuchard <grapheneowns@gmail.com>
2022-04-19 07:05:18 -0400

    update and fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "ini" {
-  value       = "TODO"
+  value       = contains(var.formats, "ini") ? file("${path.root}/${var.prefix}inventory.ini") : ""
   description = "The Ansible INI format inventory content."
 }
 
 output "yaml" {
-  value       = yamlencode(local.instances_transform)
+  value       = contains(var.formats, "yaml") ? file("${path.root}/${var.prefix}inventory.yaml") : ""
   description = "The Ansible YAML format inventory content."
 }
 
 output "json" {
-  value       = jsonencode(local.instances_transform)
+  value       = contains(var.formats, "json") ? file("${path.root}/${var.prefix}inventory.json") : ""
   description = "The Ansible JSON format inventory content."
 }
