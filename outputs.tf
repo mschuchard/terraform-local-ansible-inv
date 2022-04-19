763ea6d254795853828babfa27a0606e0296532d
Matt Schuchard <grapheneowns@gmail.com>
2022-04-19 09:15:43 -0400

    update ci versions and revert outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,26 @@
 output "ini" {
-  value       = contains(var.formats, "ini") ? file("${path.root}/${var.prefix}inventory.ini") : ""
+  value = contains(var.formats, "ini") ? templatefile(
+    "${path.module}/templates/inventory.ini.tmpl",
+    { instances = local.instances_transform }
+  ) : ""
   description = "The Ansible INI format inventory content."
+  depends_on  = [local_file.ansible_inventory]
 }
 
 output "yaml" {
-  value       = contains(var.formats, "yaml") ? file("${path.root}/${var.prefix}inventory.yaml") : ""
+  value = contains(var.formats, "yaml") ? templatefile(
+    "${path.module}/templates/inventory.yaml.tmpl",
+    { instances = local.instances_transform }
+  ) : ""
   description = "The Ansible YAML format inventory content."
+  depends_on  = [local_file.ansible_inventory]
 }
 
 output "json" {
-  value       = contains(var.formats, "json") ? file("${path.root}/${var.prefix}inventory.json") : ""
+  value = contains(var.formats, "json") ? templatefile(
+    "${path.module}/templates/inventory.json.tmpl",
+    { instances = local.instances_transform }
+  ) : ""
   description = "The Ansible JSON format inventory content."
+  depends_on  = [local_file.ansible_inventory]
 }
