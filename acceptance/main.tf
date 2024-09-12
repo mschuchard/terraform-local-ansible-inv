84bd99f129900f566debb0c5f87a95f361a2c3ff
Matt Schuchard <grapheneowns@gmail.com>
2024-09-12 09:12:22 -0400

    update accept test deps and centralize config

--- acceptance/main.tf
+++ acceptance/main.tf
@@ -27,6 +27,23 @@ resource "terraform_data" "inventory_validation" {
   }
 }
 
+# vars for dynamic variation of parameters
+variable "formats" { type = set(any) }
+
+variable "instances_var" {}
+
+# mock platform instances
+variable "instances_aws" {}
+variable "instances_gcp" {}
+variable "instances_azr" {}
+variable "instances_vsp" {}
+
+# mock group vars
+variable "group_vars" {}
+
+# mock extra provider instance hostvars
+variable "extra_hostvars" {}
+
 # inspect outputs
 output "inventory_ini" {
   value = module.ansible_inv.ini
